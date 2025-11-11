0acfa7154115b303af20c81cb397a91a08408f1c
Nik Popov <149531476+nikwif@users.noreply.github.com>
2025-11-11 16:01:31 +0000

    core

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,7 +2,7 @@ import path from "node:path"
 import { TanStackRouterVite } from "@tanstack/router-vite-plugin"
 import react from "@vitejs/plugin-react-swc"
 import { defineConfig } from "vite"
-import { nodePolyfills } from "vite-plugin-node-polyfills" // Use named import
+import { nodePolyfills } from "vite-plugin-node-polyfills"
 
 export default defineConfig({
   plugins: [
@@ -11,13 +11,7 @@ export default defineConfig({
       routesDirectory: "./src/routes",
       generatedRouteTree: "./src/routeTree.gen.ts",
     }),
-    nodePolyfills({
-      // Use the named export here
-      globals: {
-        Buffer: true, // Enable Buffer polyfill
-        global: true, // Enable global polyfill
-      },
-    }),
+    nodePolyfills(),
   ],
   resolve: {
     alias: {
